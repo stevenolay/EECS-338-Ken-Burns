9ebe0cd383c92e0a6697095500c7cc44369d73e3
samirjoshi <samirjoshi2018@u.northwestern.edu>
2016-10-07 01:05:28 -0500

    wiggle 3,6 and wiggle 2,7 videos added - need to pick the best one

--- video_creation/auto_ken.py
+++ video_creation/auto_ken.py
@@ -42,10 +42,9 @@ def random_bounding_box(w_px, h_px):
     return [box_x, box_y, box_width, box_height]
 
 #(2,6) or (3,7)
-def vid_wiggle(box_arr, freq = 3, move_scale = 7):
+def vid_wiggle(box_arr, freq = 2, move_scale = 7):
     num_effects = 24 * freq
     num_frames_per_effect = int(len(box_arr) / num_effects)
-    effects = []
     for i in range(0, num_effects):
         effect_x = int((float(random.randint(0, 100)) / float(100)) * float(move_scale))
         effect_y = int((float(random.randint(0, 100)) / float(100)) * float(move_scale))
