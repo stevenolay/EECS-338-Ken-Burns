a65c786782db8e9cbd229c50cfee9a12dd2fe896
Steven <stevenzmanx@u.northwestern.edu>
2016-11-28 22:19:34 -0600

    adding

---
+++ kenburns.py
@@ -0,0 +1,366 @@
+# -*- coding: utf-8 -*-
+import sys
+from sys import path
+import ast
+import json
+import string
+import copy
+import io
+import random
+
+#Imports for Audio and Images
+from gtts import gTTS
+from py_bing_search import PyBingImageSearch
+
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify, send_from_directory
+
+from jinja2 import Environment, FileSystemLoader
+import os, glob
+
+import re, urllib2
+
+import requests
+import random
+
+# EXTERNAL API IMPORTS
+import wikipedia
+from simple_wikipedia import *
+from Summary import *
+from video_creation import video_maker
+#from video_maker import *
+
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+MEDIA_FOLDER = 'video_creation/output_videos'
+
+# BING_API_KEY = 'wrtWDMR91PXsFFEHRYN1ZQSObkZMFvHJRljl6zyNiCI'
+BING_API_KEY = 'b4lZYRDZJ/ya1EhblueNnTukTRxUBArSz66fIfz0lys'
+
+IMAGE_FILTER = 'Size:Large+Color:Color+Style:Photo'
+
+# create our little application :)
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_vanilla_home():
+    return render_template('index.html') #Loads Welcome Page
+
+
+@app.route('/fetch/<param>')
+def fetchBio(param):
+
+    tempParam = "Barack Obama"
+
+    split_name = param.split(' ')
+    vid_name = ''
+    for i in range(0, len(split_name)):
+        vid_name = vid_name + str(split_name[i])
+        if i != (len(split_name) - 1):
+            vid_name = vid_name + '_'
+
+    vid_name = vid_name + '_test.mp4'
+    #check if the video of the person exists already -> then don't make video
+    vid_exists = False
+    curr_dir = os.getcwd()
+    dir_mp4 = str(curr_dir) + '/static/output_videos/*.mp4'
+    vids = glob.glob(dir_mp4)
+    for f_name in vids:
+        # print "NAME:" + str(f_name)
+        if vid_name in f_name:
+            print "TRUUUUUUUUUUu"
+            vid_exists = True
+            return vid_name
+    print "Did not find previously made video - making new bio"
+
+
+
+    earlyEdu = generateEarlyEdu(param)
+    earlyEdu.insert(0, "Early Life")
+
+    pageContent = parseContent(param)
+
+    earlyWork = generateEarlyWork(pageContent)
+    earlyWork.insert(0, "Early Work")
+
+    officesHeld = generateOfficesHeld(pageContent)
+    officesHeld.insert(0, "Offices Held")
+    
+    summarized = []
+
+    # for i in range(0, len(pageContent)):
+    #     try:
+    #         summarized.append([ pageContent[i][0] , summarize(pageContent[i][1]) ])
+    #     except:
+    #         continue
+
+    summarized.insert(0, earlyEdu)
+    summarized.insert(1, earlyWork)
+    summarized.insert(2, officesHeld)
+
+    return callVideoMaker(param, summarized)
+
+def callVideoMaker(name, content):
+
+    id = 0
+    #print "Content length: " + str(content)
+    arr_audio = []
+    total_text = ''
+    for each in content:
+        if each[1] == '':
+            each[1] = '.'
+        try:
+            tts = gTTS(text = each[1], lang = 'en-uk')
+            total_text = total_text + str(each[1])
+            #print each[1]
+        except:
+            #print each
+            continue
+
+        save_string = "video_creation/audio/%s"%(name + str(id)+".mp3")
+        tts.save(save_string)
+
+        arr_audio.append(name + str(id)+".mp3")
+        id += 1
+
+    #print total_text
+    try:
+        tts = gTTS(text = total_text, lang = 'en-uk')
+    except:
+        print "Total text error"
+
+    save_string = "video_creation/audio/%s"%(name + "_total.mp3")
+    tts.save(save_string)
+    arr_audio.append(name + "_total.mp3")
+
+
+    arr_arr_images = []
+    for each in content:
+        topic = each[0]
+
+        query_topic = name + " " + topic
+
+        bing_image = PyBingImageSearch(BING_API_KEY, query_topic, image_filters= IMAGE_FILTER)
+        first_fifty_result= bing_image.search(limit=7, format='json') #1-50
+        #print "DOES BING RETURN ANYTHING????" + str(first_fifty_result)
+
+        media = [x.media_url for x in first_fifty_result]
+
+        arr_arr_images.append(media)
+    vid_res = ''
+    vid_res = video_maker.make_total_vid(name, arr_arr_images, arr_audio)
+    #try:
+    #    vid_res = video_maker.make_total_vid(name, arr_arr_images, arr_audio)
+    #except:
+
+
+    video_maker.delete_audio_files()
+    
+    return vid_res
+#def make_total_vid(name, arr_arr_images, arr_audio):
+
+def summarize(content):
+
+    fs = FrequencySummarizer()
+    return str(fs.summarize(content, 2))
+
+def generateEarlyEdu(param):
+    simple_page = simple_wikipedia.page(param)
+    content = simple_page.content
+    content = string.replace(content, "====", '|')
+    content = string.replace(content, "===", '|')
+    content = string.replace(content, "==", '|')
+    content = string.replace(content, "=", '|')
+    content =  ''.join([i if ord(i) < 128 else ' ' for i in content])
+
+    firstSplit = content.split("|")
+    pageContent = []
+    eduEarlyIndexs = []
+    pair = 0
+    for i in range(1,len(firstSplit), 2):
+
+        topic = firstSplit[i].strip()
+        if re.match('early',topic, re.IGNORECASE):
+            eduEarlyIndexs.append(pair)
+        if re.match('education',topic, re.IGNORECASE):
+            eduEarlyIndexs.append(pair)
+
+        content = firstSplit[i+1].strip()
+        pageContent.append([topic.encode('ascii', 'ignore'), content.encode('ascii', 'ignore')])
+        pair += 1
+
+    retContent = [""+re.sub('\[[^)]*\]', "", pageContent[x][1]) for x in eduEarlyIndexs]
+    singular = ""
+    for each in retContent:
+        singular += each
+    return [singular]
+
+def parseContent(param):
+
+    page = wikipedia.page(param)
+    content = page.content
+
+    content = string.replace(content, "====", '|')
+    content = string.replace(content, "===", '|[Parent Node]')
+    content = string.replace(content, "==", '|[Parent Node]')
+    content = string.replace(content, "=", '|')
+    content =  ''.join([i if ord(i) < 128 else ' ' for i in content])
+
+    firstSplit = content.split("|")
+
+    pageContent = []
+
+    for i in range(1,len(firstSplit), 2):
+        topic = firstSplit[i].strip()
+        content = firstSplit[i+1].strip()
+        pageContent.append([topic.encode('ascii', 'ignore'), content.encode('ascii', 'ignore')])
+    return pageContent
+
+def generateEarlyWork(content):
+    workRelatedString = ""
+    keyWords = ['study','studied' ,'employ', 'work', 'career', 'taught', 'directed', 'elected', 'director', 'teacher', 'professor', 'lawyer', 'worker', 'employer', 'intern', 'internship']
+    contentKeys = ['career', 'early']
+    sectionIndex = 0
+    for section in content:
+        #print section[0], re.match("\[Parent Node\]",section[0], re.IGNORECASE)
+        chainOut = False;
+        if re.match("\[Parent Node\]",section[0], re.IGNORECASE):
+            for cont in contentKeys:
+                for substr in section[0].split():
+                    if re.match(cont,substr, re.IGNORECASE):
+                        workRelatedString += naiveSearch(content, sectionIndex, keyWords)
+                        chainOut = True
+                        break
+                if chainOut:
+                    break
+
+        sectionIndex += 1
+
+    workRelatedString = removeTags(workRelatedString, ["\[Parent Node\]"])
+    workRelatedString = removeDuplicates(workRelatedString)
+    return [workRelatedString.strip()]
+
+def generateOfficesHeld(content):
+    relatedString = ""
+    keyWords = ['ran', 'elected', 'served', 'office', 'seek', 'seeking', 'election', 'race', 'won', 'win', 'loss', 'held', 'hold', 'senator', 'senate']
+    contentKeys = ['election', 'legislation', 'representatives', 'legislative', 'senate', 'senator', 'representative']
+    sectionIndex = 0
+    for section in content:
+        #print section[0], re.match("\[Parent Node\]",section[0], re.IGNORECASE)
+        chainOut = False;
+        if re.match("\[Parent Node\]",section[0], re.IGNORECASE):
+            for cont in contentKeys:
+                for substr in section[0].split():
+                    if re.match(cont,substr, re.IGNORECASE):
+                        relatedString += naiveSearch(content, sectionIndex, keyWords)
+                        chainOut = True
+                        break
+                if chainOut:
+                    break
+
+        sectionIndex += 1
+
+    relatedString = removeTags(relatedString, ["\[Parent Node\]"])
+    relatedString = removeDuplicates(relatedString)
+    return [relatedString.strip()]
+
+def naiveSearch(content, sectionIndex, triggers):
+    retScentence = ""
+
+    scentences = split_into_sentences(content[sectionIndex][1])
+    for scentence in scentences:
+        for key in triggers:
+            if findWholeWord(key)(scentence):
+                retScentence += scentence
+
+    childIndices = findChildren(content, sectionIndex)
+    for child in childIndices:
+        scentences = split_into_sentences(content[child][1])
+        for scentence in scentences:
+            for key in triggers:
+                if findWholeWord(key)(scentence):
+                    retScentence += scentence
+
+    return retScentence
+
+def findChildren(content, sectionIndex):
+    childIndices = []
+    for i in range(sectionIndex + 1, len(content)):
+        if re.match("\[Parent Node\]",content[i][0], re.IGNORECASE):
+            #print childIndices
+            return childIndices
+        else:
+            childIndices.append(i)
+
+
+
+#Globals for Scentence Splitting
+caps = "([A-Z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+digits = "([0-9])"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + caps + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(caps + "[.]" + caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + caps + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+def removeTags(content, tags):
+    newContent = content
+    for tag in tags:
+        m = re.search(tag, newContent, re.IGNORECASE)
+        while m:        
+            newContent = newContent[:m.start()] + newContent[m.end():]
+            m = re.search(tag, newContent, re.IGNORECASE)
+    return newContent 
+
+def removeDuplicates(content):
+    retContent = split_into_sentences(content)
+    unique = []
+    [unique.append(item) for item in retContent if item not in unique]
+
+    return " ".join(unique)
+
+# fetchBio("obama")
+# fetchBio("bill clinton")
+# fetchBio("hillary clinton")
+# fetchBio("tim kaine")
+# fetchBio("mike pence")
+if __name__ == '__main__':
+    #app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+	app.run(debug=True)
---
+++ video_creation/video_maker.py
@@ -0,0 +1,57 @@
+import math, time, sys, os
+from ken_audio_video import *
+from auto_ken_runner import *
+
+def make_total_vid(name, arr_arr_images, arr_audio):
+    # name  = "Barack Obama"
+    # CHANGE THIS HERE TO GET LONGER VIIDEOSSSSSS
+    # FIX MULTIPLE AUDIO PROBLEM
+    # arr_arr_images = [['obama.jpg', 'obama_3.jpg'], ['obama_4.jpg', 'obama_5.jpg']]
+    # print "arr_arr_image: BEFORE" + str(arr_arr_images)
+    # arr_arr_images = [arr_arr_images[0], arr_arr_images[1], arr_arr_images[2], arr_arr_images[3], arr_arr_images[4], arr_arr_images[5]]
+    # print "arr_arr_image:" + str(arr_arr_images)
+    total_aud = arr_audio[len(arr_audio) - 1]
+    arr_audio = arr_audio[0:len(arr_audio) - 1]
+
+    split_name = name.split(' ')
+    vid_name = ''
+    for i in range(0, len(split_name)):
+        vid_name = vid_name + str(split_name[i])
+        if i != (len(split_name) - 1):
+            vid_name = vid_name + '_'
+
+    vid_only_name = vid_name + '_vid.mp4'
+    aud_only_name = vid_name + '_aud.mp4'
+    vid_name = vid_name + '_test.mp4'
+
+    print "Video Name: " + vid_name
+    print "Video Only Name: " + vid_only_name
+
+    #call thing to make the video
+    overall_runner(vid_only_name, arr_arr_images, arr_audio)
+
+    #combine all audio into a single audio file
+    concat_audio(vid_only_name, [total_aud], vid_name)
+
+    delete_audio_files()
+
+    return vid_name
+
+def delete_audio_files():
+    dp = 'video_creation/audio/'
+    f_list = os.listdir(dp)
+    for f_name in f_list:
+        os.remove(dp + f_name)
+    print "all audio files removed"
+
+    dp = 'video_creation/videos_no_audio/'
+    f_list = os.listdir(dp)
+    for f_name in f_list:
+        os.remove(dp + f_name)
+    print "all videos_no_audio files removed"
+    return 0
+
+if __name__ == '__main__':
+    # all_ken_runner(['obama.jpg', 'obama_1.jpg', 'obama_2.jpg', 'obama_3.jpg', 'obama_4.jpg', 'obama_5.jpg'], [12, 10, 13, 10, 12, 14], output_fname)
+    # make_total_vid("Barack Obama", [['obama.jpg', 'obama_3.jpg'], ['obama_4.jpg', 'obama_5.jpg']], ["jimmy2.mp3", 'jimmy2.mp3'])
+    delete_audio_files()
